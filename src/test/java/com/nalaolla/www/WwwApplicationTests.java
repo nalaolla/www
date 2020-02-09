e8160ebb23156cdd717f415109e229bb09585d74
djkim99 <djkim99@lotte.net>
2020-02-09 23:32:15 +0900

    project create

---
+++ src/test/java/com/nalaolla/www/WwwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nalaolla.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
