a14e564c28d459d225024c5feb927a2f403a0f67
djkim99 <djkim99@lotte.net>
2020-02-09 23:48:34 +0900

    project create

--- src/main/java/com/nalaolla/www/WwwApplication.java
+++ src/main/java/com/nalaolla/www/WwwApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WwwApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WwwApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(WwwApplication.class, args);
+	}
 
 }
