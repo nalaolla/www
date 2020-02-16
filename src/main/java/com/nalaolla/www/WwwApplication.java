8cb150f04020914e5e3e867026acf3bea6409982
nalaolla <60847008+nalaolla@users.noreply.github.com>
2020-02-16 14:42:50 +0900

    social login 적용

--- src/main/java/com/nalaolla/www/WwwApplication.java
+++ src/main/java/com/nalaolla/www/WwwApplication.java
@@ -2,8 +2,10 @@ package com.nalaolla.www;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
 @EnableJpaAuditing  //JPA Auditing 어노테이션들을 모두 활성화
 @SpringBootApplication
@@ -12,6 +14,11 @@ public class WwwApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WwwApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
 	}
 
 }
